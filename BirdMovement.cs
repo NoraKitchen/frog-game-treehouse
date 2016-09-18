908a144f1c097d629545d378e195fb6f2ec9d50c
NoraKitchen <narwhaltortellini@gmail.com>
2016-09-18 12:58:02 -0600

    bird follows frog*

--- BirdMovement.cs
+++ BirdMovement.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class BirdMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    [SerializeField]
+    private Transform target;
+
+    private NavMeshAgent birdAgent;
+
+    private Animator birdAnimator;
+
+
+    // Use this for initialization
+    void Start () {
+        birdAgent = GetComponent<NavMeshAgent>();
+        birdAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        //tell bird what to follow
+        birdAgent.SetDestination(target.position);
+
+        //measure magnitude of navmehsagent's velocity
+        float speed = birdAgent.velocity.magnitude;
+
+        //pass speed to animator so it knows to animate bird
+        birdAnimator.SetFloat("Speed", speed);
 	
 	}
 }
